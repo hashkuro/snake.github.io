8ee70c7fcea4263974563e647513a25727a072b6
Rupan Sardar <rupan99sardar@gmail.com>
2022-06-29 18:13:08 +0530

    beta
    changed a lot of stuffs, still can improve

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //sounds
 let eat = new Audio("res/eat.wav");
 let crash = new Audio("res/crash.wav");
+let speedup = new Audio("res/speedUp.wav");
 
-//consts and variabbles
+//const and variables
 let inputDir = { x: 0, y: 0 };
-let speed = 6;
+let speed = 5;
+let score = 0;
+let hiscore = 0;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 11, y: 11 }];
 food = { x: 3, y: 4 };
@@ -12,7 +15,6 @@ food = { x: 3, y: 4 };
 //function
 function main(ctime) {
   window.requestAnimationFrame(main);
-  //console.log(ctime);
   if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return;
   }
@@ -36,13 +38,55 @@ function gameEngine() {
   if (isCollide(snakeArr)) {
     inputDir = { x: 0, y: 0 };
     crash.play();
-    alert("GameOver!");
+    alert("Game Over!\r\nScore: " + score);
     snakeArr = [{ x: 11, y: 11 }];
     score = 0;
+    speed = 5;
+    scoreBox.innerHTML = "Hi Score:" + hiscore + "<br>" + "Score: 0";
+    instructions.innerHTML = "Press any arrow<br>key to begin";
   }
 
   if (snakeArr[0].y == food.y && snakeArr[0].x == food.x) {
     eat.play();
+    score += 100;
+    if (score % 500 == 0) {
+      speedup.play();
+    }
+    if (score > hiscore) {
+      hiscore = score;
+    }
+    //couldn't think of a better way to do this at that time, sorry
+    if (score == 500) {
+      speed = speed + 1;
+    } else if (score == 1000) {
+      speed = speed + 1;
+    } else if (score == 1500) {
+      speed = speed + 1;
+    } else if (score == 2000) {
+      speed = speed + 1;
+    } else if (score == 2500) {
+      speed = speed + 1;
+    } else if (score == 3000) {
+      speed = speed + 1;
+    } else if (score == 3500) {
+      speed = speed + 1;
+    } else if (score == 4000) {
+      speed = speed + 1;
+    } else if (score == 4500) {
+      speed = speed + 1;
+    } else if (score == 5000) {
+      speed = speed + 1;
+    }
+    scoreBox.innerHTML = "Hi Score:" + hiscore + "<br>" + "Score :" + score;
+
+    //changing the message
+    if (score >= 500) {
+      instructions.innerHTML = "The speed increases<br> with every 500 points";
+    }
+    if (score >= 1000) {
+      instructions.innerHTML = "Have Fun";
+    }
+
     snakeArr.unshift({
       x: snakeArr[0].x + inputDir.x,
       y: snakeArr[0].y + inputDir.y,
---
+++ readMe.txt
@@ -0,0 +1,11 @@
+
+
+CREDITS
+color palette: Alien Space Color Palette (with slight modification)
+credit: sunnyxscorpio
+source: https://www.color-hex.com/color-palette/1015007
+
+font: Arcade Classic
+source: https://www.dafont.com/arcade-classic-2.font
+
+sounds credit: LittleRobotSoundFactory (https://freesound.org/people/LittleRobotSoundFactory/)
